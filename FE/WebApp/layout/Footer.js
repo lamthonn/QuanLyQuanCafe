046ac64bab1c8805463b69cdbb6c2c2360b1cce9
dinhhai130903 <thihai.dinh@monkey.edu.vn>
2023-12-17 23:51:35 +0700

    quan ly ban va trang chu

--- FE/WebApp/layout/Footer.js
+++ FE/WebApp/layout/Footer.js
@@ -1,196 +1,115 @@
 const footer = document.querySelector("#footer");
 
 const content = `
-<footer
-class="text-center text-lg-start text-white"
-style="background-color: #3e4551"
->
+<footer class="text-center text-lg-start text-white" style="background-color: #3e4551">
 <!-- Grid container -->
 <div class="container p-4 pb-0">
-<!-- Section: Links -->
-<section class="">
-<!--Grid row-->
-<div class="row">
-<!--Grid column-->
-<div class="col-lg-4 col-md-6 mb-4 mb-md-0">
-  <h5 class="text-uppercase">FOOTER CONTENT</h5>
+    <!-- Section: Links -->
+    <section class="">
+        <!--Grid row-->
+        <div class="row">
+            <!--Grid column-->
+            <div class="col-lg-4 col-md-6 mb-4 mb-md-0">
+                <p>
+                    Công ty cổ phần thương mại dịch vụ Trà Cà Phê VN <br>
+                    Mã số DN: 0312867172 do sở kế hoạch và đầu tư tp.<br>
+                    Địa chỉ: 86-88 Cao Thắng, phường 04, quận 3, tp Hồ Chí Minh  <br>
+                    © 2014-2022 Công ty cổ phần thương mại dịch vụ Trà Cà Phê VN mọi quyền bảo lưu
+                </p>
+            </div>
+            <!--Grid column-->
 
-  <p>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-    Molestiae modi cum ipsam ad, illo possimus laborum ut
-    reiciendis obcaecati. Ducimus, quas. Corrupti, pariatur eaque?
-    Reiciendis assumenda iusto sapiente inventore animi?
-  </p>
-</div>
-<!--Grid column-->
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                <ul class="list-unstyled mb-0">
+                    <li>
+                    <a href="#!" class="text-white"><i class="fa-solid fa-phone"></i> Đặt hàng: 1800 6936</a>
+                    </li>
+                    <li>
+                    <a href="#!" class="text-white"><i class="fa-solid fa-location-dot"></i>Liên hệ</a><br>
+                    <p>Tầng 3-4 Hub Building <br> 195/10E Điện Biên Phủ, P.15,<br> Q.Bình Thạnh, TP.Hồ Chí Minh</p>
+                    </li>
+                    <li>
+                </ul>
+            </div>
+            <!--Grid column-->
 
-<!--Grid column-->
-<div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-  <h5 class="text-uppercase">Links</h5>
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Giới thiệu</h5>
 
-  <ul class="list-unstyled mb-0">
-    <li>
-      <a href="#!" class="text-white">Link 1</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 2</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 3</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 4</a>
-    </li>
-  </ul>
-</div>
-<!--Grid column-->
+            <ul class="list-unstyled mb-0">
+                <li>
+                <a href="#!" class="text-white">Về chúng tôi</a>
+                </li>
+                <li>
+                <a href="#!" class="text-white">Sản phẩm</a>
+                </li>
+                <li>
+                <a href="#!" class="text-white">Khuyến mãi</a>
+                </li>
+                <li>
+                <a href="#!" class="text-white">Chuyện cà phê</a>
+                </li>
+            </ul>
+            </div>
+            <!--Grid column-->
 
-<!--Grid column-->
-<div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-  <h5 class="text-uppercase">Links</h5>
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+            <h5 class="text-uppercase">Điều khoản</h5>
 
-  <ul class="list-unstyled mb-0">
-    <li>
-      <a href="#!" class="text-white">Link 1</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 2</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 3</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 4</a>
-    </li>
-  </ul>
-</div>
-<!--Grid column-->
+            <ul class="list-unstyled mb-0">
+                <li>
+                <a href="#!" class="text-white">Điều khoản sử dụng</a>
+                </li>
+                <li>
+                <a href="#!" class="text-white">Chính sách bảo mật thông tin</a>
+                </li>
+                <li>
+                <a href="#!" class="text-white">Hướng dẫn xuất hóa đơn GTGT</a>
+                </li>
+            </ul>
+            </div>
+            <!--Grid column-->
 
-<!--Grid column-->
-<div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-  <h5 class="text-uppercase">Links</h5>
+            <!--Grid column-->
+            <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                <div class="uiScaledImageContainer _2zfr" style="width:270px;height:130px;"><img class="scaledImageFitHeight img" src="https://scontent.xx.fbcdn.net/v/t39.30808-6/408254425_367812245609892_6106953464367600530_n.jpg?stp=dst-jpg_p130x130&amp;_nc_cat=102&amp;ccb=1-7&amp;_nc_sid=081abc&amp;_nc_ohc=TE930MqObSoAX8hCfOs&amp;_nc_ht=scontent.xx&amp;edm=AOm7FCUEAAAA&amp;oh=00_AfCd_2C_-N-vhilGr3shpVJwSjX9e_0VWj3ENQ1WAnQNDg&amp;oe=6583E06B" data-src="https://scontent.xx.fbcdn.net/v/t39.30808-6/408254425_367812245609892_6106953464367600530_n.jpg?stp=dst-jpg_p130x130&amp;_nc_cat=102&amp;ccb=1-7&amp;_nc_sid=081abc&amp;_nc_ohc=TE930MqObSoAX8hCfOs&amp;_nc_ht=scontent.xx&amp;edm=AOm7FCUEAAAA&amp;oh=00_AfCd_2C_-N-vhilGr3shpVJwSjX9e_0VWj3ENQ1WAnQNDg&amp;oe=6583E06B" style="left:-31px;" alt="Ảnh của The Coffee House." width="300" height="130" caption="Ảnh của The Coffee House."></div>
+            </div>
+                <!--Grid column-->
+        </div>
+        <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+    <hr class="mb-4" />
 
-  <ul class="list-unstyled mb-0">
-    <li>
-      <a href="#!" class="text-white">Link 1</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 2</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 3</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 4</a>
-    </li>
-  </ul>
-</div>
-<!--Grid column-->
+    <!-- Section: Social media -->
+    <section class="mb-4 text-center">
+        <!-- Facebook -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" ><i class="fab fa-facebook-f"></i></a>
 
-<!--Grid column-->
-<div class="col-lg-2 col-md-6 mb-4 mb-md-0">
-  <h5 class="text-uppercase">Links</h5>
+        <!-- Twitter -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" ><i class="fab fa-twitter"></i ></a>
 
-  <ul class="list-unstyled mb-0">
-    <li>
-      <a href="#!" class="text-white">Link 1</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 2</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 3</a>
-    </li>
-    <li>
-      <a href="#!" class="text-white">Link 4</a>
-    </li>
-  </ul>
-</div>
-<!--Grid column-->
-</div>
-<!--Grid row-->
-</section>
-<!-- Section: Links -->
+        <!-- Google -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" ><i class="fab fa-google"></i ></a>
 
-<hr class="mb-4" />
+        <!-- Instagram -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" ><i class="fab fa-instagram"></i ></a>
 
-<!-- Section: CTA -->
-<section class="">
-<p class="d-flex justify-content-center align-items-center">
-<span class="me-3">Register for free</span>
-<button type="button" class="btn btn-outline-light btn-rounded">
-  Sign up!
-</button>
-</p>
-</section>
-<!-- Section: CTA -->
+        <!-- Linkedin -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" ><i class="fab fa-linkedin-in"></i ></a>
 
-<hr class="mb-4" />
-
-<!-- Section: Social media -->
-<section class="mb-4 text-center">
-<!-- Facebook -->
-<a
- class="btn btn-outline-light btn-floating m-1"
- href="#!"
- role="button"
- ><i class="fab fa-facebook-f"></i
-></a>
-
-<!-- Twitter -->
-<a
- class="btn btn-outline-light btn-floating m-1"
- href="#!"
- role="button"
- ><i class="fab fa-twitter"></i
-></a>
-
-<!-- Google -->
-<a
- class="btn btn-outline-light btn-floating m-1"
- href="#!"
- role="button"
- ><i class="fab fa-google"></i
-></a>
-
-<!-- Instagram -->
-<a
- class="btn btn-outline-light btn-floating m-1"
- href="#!"
- role="button"
- ><i class="fab fa-instagram"></i
-></a>
-
-<!-- Linkedin -->
-<a
- class="btn btn-outline-light btn-floating m-1"
- href="#!"
- role="button"
- ><i class="fab fa-linkedin-in"></i
-></a>
-
-<!-- Github -->
-<a
- class="btn btn-outline-light btn-floating m-1"
- href="#!"
- role="button"
- ><i class="fab fa-github"></i
-></a>
-</section>
-<!-- Section: Social media -->
+        <!-- Github -->
+        <a class="btn btn-outline-light btn-floating m-1" href="#!" role="button" ><i class="fa-solid fa-store"></i></a>
+    </section>
+    <!-- Section: Social media -->
 </div>
 <!-- Grid container -->
 
 <!-- Copyright -->
-<div
-class="text-center p-3"
-style="background-color: rgba(0, 0, 0, 0.2)"
->
-© 2020 Copyright:
-<a class="text-white" href="https://mdbootstrap.com/"
->MDBootstrap.com</a
->
-</div>
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)" >© 2020 Copyright:thecoffehouse</a></div>
 <!-- Copyright -->
 </footer>
 `
--- FE/WebApp/layout/Header.js
+++ FE/WebApp/layout/Header.js
@@ -1,50 +1,65 @@
 const header = document.querySelector("#header");
 
 const contentHeader = `
-<nav class="navbar navbar-expand-lg bg-light navbar-light">
-<button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-    data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-    aria-label="Toggle navigation">
-    <i class="fas fa-bars"></i>
-</button>
-<div class="container d-flex justify-content-center">
-    <div class="row">
-        <div class="col-12 d-flex justify-content-center mb-3">
-            <a class="navbar-brand" href="#"><img id="MDB-logo"
-                    src="https://mdbcdn.b-cdn.net/wp-content/uploads/2018/06/logo-mdb-jquery-small.png"
-                    alt="MDB Logo" draggable="false" height="30" /></a>
-        </div>
-        <div class="col-12 d-flex justify-content-center">
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav align-items-center mx-auto">
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="./Home.html"><i class="fa-solid fa-home"></i>Trang
-                            chủ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="./SanPham.html"><i
-                                class="fa-solid fa-wine-bottle"></i>Quản lý đồ uống</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="#!"><i class="fa-solid fa-table"></i>Quản lý bàn</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="#!"><i class="fa-solid fa-users"></i>Quản lý nhân
-                            viên</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="#!"><i class="fa-solid fa-money-bill"></i>Quản lý hóa
-                            đơn</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-2" href="#!"><i class="fa-regular fa-calendar-check"></i>Quản lý
-                            ca trực</a>
-                    </li>
-                </ul>
+<div class="top-bar">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                        <div class="header-meta-list">
+                            <div class="location-header">
+                                <a href="/"><i class="fa-solid fa-location-dot icon-location"></i><span>146 Cửa hàng khắp cả nước</span></a>
+                            </div>
+                            <div>
+                                <a href="/"><i class="fa-solid fa-phone icon-phone"></i><span>Đặt hàng: 1800.6936</span></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-</div>
-</nav>
+        <nav class="navbar navbar-expand-lg bg-light navbar-light">
+            <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
+                data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="container d-flex justify-content-center">
+                <div class="row">
+                    <div class="col-12 d-flex justify-content-center">
+                        <a class="navbar-brand" href="#"><img id="MDB-logo"
+                                src="../assets/images/logo.png"
+                                alt="Logo The Coffe House" draggable="false" height="30" /></a>
+                    </div>
+                    <div class="col-12 d-flex justify-content-center">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav align-items-center mx-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link mx-2" href="./Home.html"><i class="fa-solid fa-home"></i>Trang
+                                        chủ</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mx-2" href="./SanPham.html"><i
+                                            class="fa-solid fa-wine-bottle"></i>Quản lý đồ uống</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mx-2" href="./QuanLyBan.html"><i class="fa-solid fa-table"></i>Quản lý bàn</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mx-2" href="#!"><i class="fa-solid fa-users"></i>Quản lý nhân
+                                        viên</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mx-2" href="#!"><i class="fa-solid fa-money-bill"></i>Quản lý hóa
+                                        đơn</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link mx-2" href="#!"><i class="fa-regular fa-calendar-check"></i>Quản lý
+                                        ca trực</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </nav>
 `
 header.innerHTML = contentHeader;
