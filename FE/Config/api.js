ce7fb43681ff91be265c7927ec3dea4b7c95d3e4
lamthonn <lamvu180903@gmail.com>
2023-12-17 15:05:47 +0700

    new code

--- FE/Config/api.js
+++ FE/Config/api.js
@@ -3,7 +3,9 @@ const api = {
     //sản phẩm
     GET_ALL_SAN_PHAM:`${path}/api/SanPham`,
     ADD_SAN_PHAM:`${path}/api/SanPham`,
-    EDIT_SAN_PHAM:`${path}/api/SanPham`
+    EDIT_SAN_PHAM:`${path}/api/SanPham`,
+    DELETE_SAN_PHAM:`${path}/api/SanPham`,
+    // SEARCH_SAN_PHAM:`${path}/api/SanPham/search`
 };
 
 export default api;
